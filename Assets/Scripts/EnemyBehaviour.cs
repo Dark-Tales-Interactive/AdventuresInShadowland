7d78948c9660ce07e74d8ecd486fb8cc94cc38f0
Feetsalot <jdtshadow@aol.com>
2015-03-21 15:11:18 -0400

    First Commit

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Target ();
+	}
+
+	void Target()
+	{
+		if (this.GetComponent<CircleCollider2D>().IsTouching(player.GetComponent<BoxCollider2D> ()))
+		{
+			Debug.Log ("Player within range");
+			//this.transform.Translate
+		}
+	}
+}
